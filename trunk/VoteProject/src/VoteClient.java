87e5e1c1bbea0c065810073eba4bae2d303aea54
jordan.osecki <jordan.osecki@39f56638-4021-11de-af2b-f15df6ab9c44>
2009-05-15 07:05:43 +0000

    Made very anal/cosmetic changes just after importing it into Eclipse and reading it all over.

--- trunk/VoteProject/src/VoteClient.java
+++ trunk/VoteProject/src/VoteClient.java
@@ -8,36 +8,38 @@ public class VoteClient
 
 	public static void main(String[] args) 
 	{
+		// Variables needed are defined here
 		String userInput;
 		String stateInput;
 		String candidateInput;
 		ArrayList<VoteServer> serverList;
 		ArrayList<String> stateList = new ArrayList<String>();
 		VoteServer clientServer = null;
-		
+
 		try 
 		{
-			//Create new server list
+			// Create new server list
 			serverList = new ArrayList<VoteServer>();
-			
-			//Get a reader to get client input from console
-    		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-						
-			//Start up 3 initial servers
+
+			// Get a reader to get client input from console
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			// Start up 3 initial servers
 			VoteServer server1 = new VoteServer("NJ");
-			VoteServer server2 = new VoteServer("NJ");
-			VoteServer server3 = new VoteServer("NJ");
-			
+			VoteServer server2 = new VoteServer("PA");
+			VoteServer server3 = new VoteServer("NY");
+
 			server1.start();
 			server2.start();
 			server3.start();
-			
+
 			serverList.add(server1);
 			serverList.add(server2);
 			serverList.add(server3);
-			
-    		do
-    		{
+
+			// Loop which runs the menu for the client until quit is chosen
+			do
+			{
 				System.out.println("\n\n----------- Menu ------------");
 				System.out.println("(1)  Vote For Candidate");
 				System.out.println("(2)  Results By Candidate");
@@ -45,79 +47,80 @@ public class VoteClient
 				System.out.println("(4)  National Tally For All Candidates");
 				System.out.println("(5)  Kill Random Server");
 				System.out.println("(6)  Quit");
-		
+				System.out.println("---------------------------------");
+
 				System.out.println();
 				System.out.print("Select An Option: ");
 				userInput = br.readLine();
 
-				//Process based on user selection
+				// Process based on user selection
 				if (userInput.equals("1"))
 				{
-					System.out.print("Select A State: ");
+					System.out.print("Select A State:  ");
 					stateInput = br.readLine();
-		
-					//add state to global state list
-					if (!stateList.contains(stateInput))
+
+					// Add state to global state list
+					if (! stateList.contains(stateInput))
 						stateList.add(stateInput);
-					
-					System.out.print("Select A Candidate: ");
+
+					System.out.print("Select A Candidate:  ");
 					candidateInput = br.readLine();					
-					
-					//Vote
+
+					// Vote
 					clientServer = new VoteServer(stateInput);
 					clientServer.vote(candidateInput);
 					serverList.add(clientServer);
 				}
 				else if (userInput.equals("2"))
 				{
-					System.out.print("Select A Candidate: ");
+					System.out.print("Select A Candidate:  ");
 					candidateInput = br.readLine();	
-					
+
 					int total = 0;
-					
-					//spawn a channel for each state and grab the candidate
+
+					// Spawn a channel for each state and grab the candidate
 					for (int i = 0; i < stateList.size(); i++)
 					{
 						clientServer = new VoteServer(stateList.get(i));
 						total = total + clientServer.getResultsByCandidate(candidateInput);
 						serverList.add(clientServer);
 					}
-					
-					System.out.println("National Results For Candidate " + candidateInput + " : " + total);
+
+					System.out.println("National Results For Candidate " + candidateInput + ":  " + total + ".");
 				}
 				else if (userInput.equals("3"))
 				{
-					System.out.print("Select A State: ");
+					System.out.print("Select A State:  ");
 					stateInput = br.readLine();				
-					
-					//Get candidates for state
+
+					// Get candidates for state
 					clientServer = new VoteServer(stateInput);
 					String tally = clientServer.getCandidatesByState();					
 					serverList.add(clientServer);
-					
-					System.out.println("All Candidate Results In " + stateInput + " : " + tally);
-					
+
+					System.out.println("All Candidate Results In " + stateInput + ":  " + tally + ".");
+
 				}
 				else if (userInput.equals("4"))
 				{
 					Hashtable<String, Integer> nationalResults = new Hashtable<String, Integer>();
-					
-					//spawn a channel for each state and grab the candidate
+
+					// Spawn a channel for each state and grab the candidate
 					for (int i = 0; i < stateList.size(); i++)
 					{
 						clientServer = new VoteServer(stateList.get(i));
 						Hashtable tempHT = clientServer.getResultsByStateHT();
 						serverList.add(clientServer);
-						
-						
-						//iterate through our temp hash table and add to national results
-						
+
+
+						// Iterate through our temp hash table and add to national results
+
 						Iterator<String> iter = tempHT.keySet().iterator();
-						
+
 						while (iter.hasNext())
 						{
 							String cand = iter.next();
-							
+
 							if (nationalResults.containsKey(cand))
 							{
 								int candidateVoteCount = nationalResults.get(cand);
@@ -130,24 +133,21 @@ public class VoteClient
 							}
 						}	
 					}
-					
-					System.out.println("National Results : " + nationalResults.toString());
+
+					System.out.println("National Results:  " + nationalResults.toString());
 				}			
 				else if (userInput.equals("5"))
 				{
+					// Randomly choose a server in the list to crash
 					int random = (int) ( 0 + Math.random() * serverList.size());
 					serverList.get(random).stop();
 				}
-    		} while(!userInput.equals("6"));
-		
-    		//stop servers
-    		
+			} while(!userInput.equals("6"));
 		} 
 		catch (Exception e) 
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("Error during initial server initiation or during menu loop.");
 		}
 	}
-
 }
--- trunk/VoteProject/src/VoteServer.java
+++ trunk/VoteProject/src/VoteServer.java
@@ -1,60 +1,52 @@
-import org.jgroups.ChannelClosedException;
-import org.jgroups.ChannelNotConnectedException;
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.ReceiverAdapter;
 import org.jgroups.View;
 import org.jgroups.util.Util;
-
-import java.util.List;
-import java.util.LinkedList;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Hashtable;
 
 public class VoteServer extends ReceiverAdapter
 {
+	// Variables used in this class
 	private String state;
-	private String candidate;
 	private JChannel channel;
-	
 	public java.util.Hashtable<String, Integer> voteTally;
-	
+
+	// Constructor 1
 	public VoteServer()
 	{
-		voteTally = new Hashtable<String, Integer>();
+		voteTally = new Hashtable<String, Integer>();	// Create data structure
 	}
 
+	// Constructor 2
 	public VoteServer(String st)
 	{
-		voteTally = new Hashtable<String, Integer>();	//create data structure
-		state = st;										//save state for channel connection
+		voteTally = new Hashtable<String, Integer>();	// Create data structure
+		state = st;										// Save state for channel connection
 	}
-	
+
 	public void start() throws Exception
 	{
 		System.out.println("\nStarting New Channel - Joining Cluster");
-		
+
 		channel = new JChannel();
 		channel.setReceiver(this);
-		channel.connect(state);							//join the channel for the state we want
+		channel.connect(state);							// Join the channel for the state we want
 		channel.getState(null, 10000);	
-		
-		//maybe start a new thread for a heartbeat?
+
+		// TODO start a new thread for a heartbeat?
 	}	
-	
+
 	public void stop()
 	{
 		channel.disconnect();
 	}
-	
+
 	public void vote(String cand) throws Exception
 	{
-		candidate = cand;
-
-		//add candidate vote to data structure and propogate
+		// Add candidate vote to data structure and propagate
 		start();
-		
+
 		if (voteTally.containsKey(cand))
 		{
 			int candidateVoteCount = voteTally.get(cand);
@@ -65,9 +57,9 @@ public class VoteServer extends ReceiverAdapter
 		{
 			voteTally.put(cand, 1);
 		}
-		
+
 		Message msg = new Message(null, null, voteTally);
-        channel.send(msg);			
+		channel.send(msg);			
 	}
 
 	public String getCandidatesByState() throws Exception
@@ -75,83 +67,82 @@ public class VoteServer extends ReceiverAdapter
 		start();
 		return voteTally.toString();
 	}
-	
+
 	public int getResultsByCandidate(String cand) throws Exception
 	{	
 		int ret = 0;
-		
+
 		start();
-		
+
 		if (voteTally.containsKey(cand))
 			ret = voteTally.get(cand);
-		
+
 		return ret;
 	}
-	
-	public Hashtable getResultsByStateHT() throws Exception
+
+	public Hashtable<String, Integer> getResultsByStateHT() throws Exception
 	{
 		start();
-		
 		return voteTally;
 	}
-	
-    public void receive(Message msg) 
-    {
-    	
-    	if (msg.getObject() instanceof Hashtable)
-    	{
-        	Hashtable rcvdVoteTally = (Hashtable)msg.getObject();
-            
-            synchronized(voteTally)
-            {
-            	voteTally = rcvdVoteTally;
-            }
-    	}
-    	else
-    	{
-    		//maybe process a string message for a heartbeat
-    	}
-    }	
-	
+
+	@SuppressWarnings("unchecked")
+	public void receive(Message msg) 
+	{
+
+		if (msg.getObject() instanceof Hashtable)
+		{
+			Hashtable<String, Integer> rcvdVoteTally = (Hashtable<String, Integer>)msg.getObject();
+
+			synchronized(voteTally)
+			{
+				voteTally = rcvdVoteTally;
+			}
+		}
+		else
+		{
+			// TODO Process a string message for a heartbeat?
+		}
+	}	
+
 	public void viewAccepted(View new_view)
 	{
-		//This is called whenever someone joins or leaves the group
-		
-		System.out.println("View Changed : " + channel.getLocalAddress().toString());
-		
-		System.out.println("Servers In Cluster " + this.state + " : " + new_view.printDetails());
+		// This is called whenever someone joins or leaves the group
+		System.out.println("View Changed:  " + channel.getLocalAddress().toString());
+		System.out.println("Servers In Cluster " + this.state + ":   " + new_view.printDetails());
 	}
-	
-    public byte[] getState()				
-    {
-        synchronized(this.voteTally) 
-        {
-            try 
-            {
-                return Util.objectToByteBuffer(this.voteTally);
-            }
-            catch(Exception e) 
-            {
-                e.printStackTrace();
-                return null;
-            }
-        }
-    }
-	
-    public void setState(byte[] new_state) 
-    {
-        try 
-        {        	
-            Hashtable tempVoteTally = (Hashtable)(Util.objectFromByteBuffer(new_state));
-            
-            synchronized(this.voteTally) 
-            {
-                this.voteTally = tempVoteTally;
-            }
-        }
-        catch(Exception e) 
-        {
-            e.printStackTrace();
-        }
-    }
+
+	public byte[] getState()				
+	{
+		synchronized(this.voteTally) 
+		{
+			try 
+			{
+				return Util.objectToByteBuffer(this.voteTally);
+			}
+			catch(Exception e) 
+			{
+				e.printStackTrace();
+				return null;
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public void setState(byte[] new_state) 
+	{
+		try 
+		{        	
+			Hashtable<String, Integer> tempVoteTally = (Hashtable<String, Integer>)(Util.objectFromByteBuffer(new_state));
+
+			synchronized(this.voteTally) 
+			{
+				this.voteTally = tempVoteTally;
+			}
+		}
+		catch(Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
